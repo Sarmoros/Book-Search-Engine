1c8d0d1dba0df7da4fe324596263ed80aec91a72
Sarahy <sarahym0703@gmail.com>
2024-03-24 11:58:19 -0400

    added a SavedBooks.jsx file and some code

---
+++ Develop/client/src/components/SavedBooks.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SavedBooks = ({ savedBooks }) => {
+  return (
+    <div>
+      <h2>Saved Books</h2>
+      <ul>
+        {savedBooks.map((book) => (
+          <li key={book.id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            {/* Additional book details */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SavedBooks;
