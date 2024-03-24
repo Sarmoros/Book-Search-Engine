6dc0dc3efa6ac454937fe1edc47b114ee112a4b3
Sarahy <sarahym0703@gmail.com>
2024-03-24 12:53:14 -0400

    added a search books query

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -5,36 +5,42 @@ type User {
     email: String
     bookCount: Int
     savedBooks: [Book]
-  }
-  type Book {
+}
+
+type Book {
     bookId: ID!
     authors: [String]
     description: String
     image: String
     link: String
     title: String!
-  }
-  type Auth {
+}
+
+type Auth {
     token: ID!
     user: User
-  }
-  input BookInput {
+}
+
+input BookInput {
     authors: [String]
     description: String!
     bookId: String!
     image: String
     link: String
     title: String!
-  }
-  type Query {
+}
+
+type Query {
     me: User
-  }
-  type Mutation {
+    searchBooks(searchTerm: String!): [Book]
+}
+
+type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: BookInput!): User
     removeBook(bookId: ID!): User
-  }
-`
+}
+`;
 
-module.exports = typeDefs
+module.exports = typeDefs;
